b1b67ac9bb34b30eec679e246245796fc1b1f562
Kavya Sreedhar <skavya@caddy02.stanford.edu>
2020-01-12 18:03:29 -0800

    memtest errs

--- EE272-HW1/test.sh
+++ EE272-HW1/test.sh
@@ -1,6 +1,6 @@
 #!/bin/bash 
-for f in examples/layer/resnet/*
+for f in examples/layer/resnet34/*
 do 
     echo $f
-    python ./tools/run_optimizer.py basic ./examples/arch/3_level_mem_basic_example.json ./$f -s ./examples/schedule/dataflow_C_K.json -v
+    python ./tools/run_optimizer.py mem_explore ./examples/arch/baseline.json ./$f -s ./examples/schedule/dataflow_C_K.json -vvv
 done
