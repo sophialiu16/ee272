0602c4c2ed76564c6a6774911e140174d8db7ab5
Kavya Sreedhar <skavya@caddy04.stanford.edu>
2020-01-13 23:05:42 -0800

    mem explore

--- EE272-HW1/test.sh
+++ EE272-HW1/test.sh
@@ -2,5 +2,5 @@
 for f in examples/layer/resnet34/*
 do 
     echo $f
-    python ./tools/run_optimizer.py basic ./examples/arch/baseline.json ./$f -s ./examples/schedule/dataflow_C_K_16.json -v
+    python ./tools/run_optimizer.py mem_explore ./examples/arch/baseline.json ./$f -s ./examples/schedule/dataflow_C_K_16.json -vv
 done
