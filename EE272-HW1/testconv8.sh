901a485f842c67148da6308fe9e534bc2faa1750
Kavya Sreedhar <skavya@caddy01.stanford.edu>
2020-01-14 23:36:58 -0800

    testing 4 and 8 pe arrays

---
+++ EE272-HW1/testconv8.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+echo *****************************
+python ./tools/run_optimizer.py mem_explore ./examples/arch/archs/baseline8_true.json ./examples/layer/resnet34/conv1.json -s ./examples/schedule/dataflow8.json -v
+
+python ./tools/run_optimizer.py mem_explore ./examples/arch/archs/baseline8.json ./examples/layer/resnet34/conv5.json -s ./examples/schedule/dataflow8.json -v
+
+python ./tools/run_optimizer.py mem_explore ./examples/arch/archs/baseline8.json ./examples/layer/resnet34/conv4.json -s ./examples/schedule/dataflow8.json -v
+
+python ./tools/run_optimizer.py mem_explore ./examples/arch/archs/baseline8.json ./examples/layer/resnet34/conv3.json -s ./examples/schedule/dataflow8.json -v
+
+python ./tools/run_optimizer.py mem_explore ./examples/arch/archs/baseline8.json ./examples/layer/resnet34/conv2.json -s ./examples/schedule/dataflow8.json -v
+
+
+
+
+
+
