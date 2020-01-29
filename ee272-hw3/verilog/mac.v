3e317a114905dd462d277b824ed2ca9fbb3ae4d9
Sophia Liu <sophliu@caddy17.stanford.edu>
2020-01-28 19:32:46 -0800

    mac

--- ee272-hw3/verilog/mac.v
+++ ee272-hw3/verilog/mac.v
@@ -14,4 +14,29 @@ module mac
   output [OFMAP_WIDTH - 1 : 0] ofmap_out
 );
 
+// logic stuff
+  logic [2*IFMAP_WIDTH - 1 : 0] input_weight_product;
+  logic [OFMAP_WIDTH - 1 : 0] ofmap_out_reg;
+  logic [IFMAP_WIDTH - 1 : 0] ifmap_out_reg;
+  // always blocks, etc.
+  
+  always_ff @(posedge clk, negedge rst_n) begin 
+    if (rst_n & enable) begin
+      input_weight_product = ifmap_in * weight_in;
+      ofmap_out_reg = ofmap_in + input_weight_product;
+    end
+  end 
+  
+  always_ff @(posedge clk, negedge rst_n) begin 
+    if (rst_n & enable) begin
+    	ifmap_out_reg = ifmap_in;
+    end
+  end
+  
+  always_comb begin
+    ifmap_out = ifmap_out_reg;
+    ofmap_out = ofmap_out_reg;
+  end 
+
 endmodule
+
