a5c2437f63ab74e6342879a645dfbf72622befb3
Sophia Liu <sophliu@caddy17.stanford.edu>
2020-01-29 18:01:07 -0800

    priyanka's updates

--- ee272-hw3/conv_gold.cpp
+++ ee272-hw3/conv_gold.cpp
@@ -21,7 +21,7 @@ void conv_gold( int16_t ifmap[(OFMAP_HEIGHT-1)*STRIDE+FILTER_SIZE][(OFMAP_WIDTH-
         ACC:for (int c=0; c < IFMAP_CHANNELS; ++c) { 
           WR: for (int fx=0; fx < FILTER_SIZE; fx++) {
             WC: for (int fy=0; fy < FILTER_SIZE; fy++) {
-              tmp += (int32_t) ifmap[STRIDE*i+fx][STRIDE*j+fy][c] * (int32_t) weights[fx][fy][c][k];
+              tmp += (int32_t) ifmap[STRIDE*i+fy][STRIDE*j+fx][c] * (int32_t) weights[fy][fx][c][k];
             }
           }
         }
