203df9e4f721ec01d4db212f1f6d9eab1a2d8d33
Sophia Liu <sophliu@caddy07.stanford.edu>
2020-02-01 11:52:25 -0800

    mac weight

--- ee272-hw3/verilog/mac.v
+++ ee272-hw3/verilog/mac.v
@@ -7,6 +7,7 @@ module mac
   input clk,
   input rst_n,
   input enable,
+  input weight_write_enable,
   input [IFMAP_WIDTH - 1 : 0] ifmap_in,
   input [WEIGHT_WIDTH - 1 : 0] weight_in,
   input [OFMAP_WIDTH - 1 : 0] ofmap_in,
@@ -18,22 +19,34 @@ module mac
   logic [2*IFMAP_WIDTH - 1 : 0] input_weight_product;
   logic [OFMAP_WIDTH - 1 : 0] ofmap_out_reg;
   logic [IFMAP_WIDTH - 1 : 0] ifmap_out_reg;
+  reg [WEIGHT_WIDTH - 1 : 0] weight_in_reg;
   // always blocks, etc.
   
   always_ff @(posedge clk, negedge rst_n) begin 
-    if(!rst_n) begin 
-    //ofmap_out_reg = 0
-    //input_weight_product0
+    if(~rst_n) begin 
+      ofmap_out_reg <= 0;
     end 
     else if (enable) begin
-      input_weight_product <= ifmap_in * weight_in;
+      input_weight_product <= ifmap_in * weight_in_reg;
       ofmap_out_reg <= ofmap_in + input_weight_product;
     end
   end 
   
   always_ff @(posedge clk, negedge rst_n) begin 
-    if (rst_n & enable) begin
+   if(~rst_n) begin 
+      ifmap_out_reg <= 0;
+   end 
+   else if (rst_n & enable) begin
     	ifmap_out_reg <= ifmap_in;
+    end
+  end
+  
+  always_ff @(posedge clk, negedge rst_n) begin
+    if(~rst_n) begin 
+      weight_in_reg <= 0;
+    end 
+    else if (weight_write_enable) begin
+    	weight_in_reg <= weight_in;
     end
   end
   
@@ -43,4 +56,3 @@ module mac
   end 
 
 endmodule
-
