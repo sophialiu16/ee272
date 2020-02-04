099780c26444ef63f1507e0a8c1c568d0415d148
Kavya Sreedhar <skavya@caddy05.stanford.edu>
2020-02-03 21:20:02 -0800

    block 3d

--- ee272-hw3/test.py
+++ ee272-hw3/test.py
@@ -9,5 +9,22 @@ def block():
 			for y in range(Y0):
 				for x in range(X0):
 					print(x + y*X + x0*X0 + y0*X*Y0, " ")
-block()
+
+def block3D():
+	C0 = 2
+	X0 = 3
+	Y0 = 4
+	C = 4
+	X = 6
+	Y = 8
+
+	for y0 in range(Y/Y0):
+		for x0 in range(X/X0):
+			for c0 in range(C/C0):
+				for y in range(Y0):
+					for x in range(X0):
+						for c in range(C0):
+							print(c + x*C + y*C*X + c0*C0 + x0*X0*C + y0*Y0*X*C)
+					
+block3D()
 
