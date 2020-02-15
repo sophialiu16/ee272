f8dfbfbb09074614c95a2a0c1f93f9ed621b5986
Kavya Sreedhar <skavya@caddy13.stanford.edu>
2020-02-14 19:29:25 -0800

    clean

--- ee272-hw4/src/SystolicArray.h
+++ ee272-hw4/src/SystolicArray.h
@@ -26,31 +26,35 @@ void run(ac_channel<Params> &paramsIn,
         // -------------------------------
 
 	Params in_params = paramsIn.read();
+	Params paramsOut_;
+        LoopIndices loopIndicesOut_;
 
-	for (oy1 = 0; oy1 < in_params.OY1; oy1++) {
-          for (ox1 = 0; ox1 < in_params.OX1; ox1++) {
-            for (oc1 = 0; oc1 < in_params.OC1; oc1++) {
-              for (ic1 = 0; ic1 < in_params.IC1; ic1++) {
-                for (fy = 0; fy < in_params.FY; fy++) {
-                  for (fx = 0; fx < in_params.FX; fx++) {
-                    loopIndicesOut.ic1_idx.write(ic1);
-                    loopIndicesOut.fx_idx.write(fx);
-                    loopIndicesOut.fy_idx.write(fy);
-                    paramsOut.OY1.write(oy1);
-                    paramsOut.OX1.write(ox1);
-                    paramsOut.OY0.write(oy0);
-                    paramsOut.OX0.write(ox0);
-                    paramsOut.OC1.write(oc1);
-                    paramsOut.IC1.write(ic1);
-                    paramsOut.FX.write(fx);
-                    paramsOut.FY.write(fy);
-                    paramsOut.STRIDE.write(in_params.STRIDE);
+	for (int oy1 = 0; oy1 < in_params.OY1; oy1++) {
+          for (int ox1 = 0; ox1 < in_params.OX1; ox1++) {
+            for (int oc1 = 0; oc1 < in_params.OC1; oc1++) {
+              for (int ic1 = 0; ic1 < in_params.IC1; ic1++) {
+                for (int fy = 0; fy < in_params.FY; fy++) {
+                  for (int fx = 0; fx < in_params.FX; fx++) {
+                    loopIndicesOut_.ic1_idx = ic1;
+                    loopIndicesOut_.fx_idx = fx;
+                    loopIndicesOut_.fy_idx = fy;
+                    paramsOut_.OY1 = oy1;
+                    paramsOut_.OX1 = ox1;
+                    paramsOut_.OY0 = in_params.OY0;
+                    paramsOut_.OX0 = in_params.OX0;
+                    paramsOut_.OC1 = oc1;
+                    paramsOut_.IC1 = ic1;
+                    paramsOut_.FX = fx;
+                    paramsOut_.FY = fy;
+                    paramsOut_.STRIDE = in_params.STRIDE;
                   }
                 }
               }
             }
           }
         }
+        paramsOut.write(paramsOut_);
+        loopIndicesOut.write(loopIndicesOut_);
         // -------------------------------
         // Your code ends here
         // -------------------------------
