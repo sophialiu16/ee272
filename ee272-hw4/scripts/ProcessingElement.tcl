2892a1ba4ecf706b8a467f86415a2d9677d53893
Sophia Liu <sophliu@caddy02.stanford.edu>
2020-02-17 21:07:41 -0800

    pe.tcl

--- ee272-hw4/scripts/ProcessingElement.tcl
+++ ee272-hw4/scripts/ProcessingElement.tcl
@@ -3,7 +3,7 @@ set blockname [file rootname [file tail [info script] ]]
 source scripts/common.tcl
 
 directive set -DESIGN_HIERARCHY { 
-    {ProcessingElement<IDTYPE, ODTYPE>} 
+    {ProcessingElement<IDTYPE, ODTYPE, WDTYPE>} 
 }
 
 go compile
@@ -16,7 +16,7 @@ directive set -CLOCKS $clocks
 
 go assembly
 
-directive set /ProcessingElement<IDTYPE,ODTYPE>/run -DESIGN_GOAL Latency
-directive set /ProcessingElement<IDTYPE,ODTYPE>/run -CLOCK_OVERHEAD 0.000000
+directive set /ProcessingElement<IDTYPE,ODTYPE,WDTYPE>/run -DESIGN_GOAL Latency
+directive set /ProcessingElement<IDTYPE,ODTYPE,WDTYPE>/run -CLOCK_OVERHEAD 0.000000
 
 go extract
