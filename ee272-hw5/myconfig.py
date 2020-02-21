0568111cc7fec23550eb7cc7ff8d10eda84b31de
Sophia Liu <sophliu@caddy17.stanford.edu>
2020-02-20 21:24:19 -0800

    config

---
+++ ee272-hw5/myconfig.py
@@ -0,0 +1,22 @@
+# Data word size
+word_size = 32
+# Number of words in the memory
+num_words = 128
+# Technology to use in £OPENRAM_TECH
+tech_name = "freepdk45"
+# Process corners to characterize
+process_corners = ["TT"]
+# Voltage corners to characterize
+supply_voltages = [ 1.1 ]
+# Temperature corners to characterize
+temperatures = [ 25 ]
+# Output directory for the results
+output_path = "temp"
+# Output file base name
+output_name = "sram_{0}_{1}_{2}".format(word_size,num_words,tech_name)
+# Disable analytical models for full characterization (WARNING: slow!)
+# analytical_delay = False
+# To force this to use calibre for DRC/LVS/PEX
+drc_name = "calibre"
+lvs_name = "calibre"
+pex_name = "calibre"
