6ded2cbe8ac9e57d45e59e4f7c28e1f9d305bc48
Kavya Sreedhar <skavya@caddy05.stanford.edu>
2020-02-21 17:18:55 -0800

    add

---
+++ ee272-hw5/Conv/design/constraints/constraints.tcl
@@ -0,0 +1,21 @@
+set clock_net clk
+set clock_name ideal_clock
+
+create_clock -name ${clockname} \
+	     -period ${dc_clock_period} \
+	     [get_ports $clock_net}]
+
+set_load -pin_load $ADK_TYPICAL_ON_CHIP_LOAD [all_output]
+
+set_driving_cell -no_design_rule \
+	-lib_cell $ADK_DRIVING_CELL [all_inputs]
+
+set_input_delay -clock ${clock_name} [expr ${dc_clock_period}/2.0] [all_inputs]
+
+set_output_delay -clock ${clock_name} 0 [all_outputs]
+
+set_max_fanout 20 $dc_design_name
+
+set_max_transition [expr 0.25*${dc_clock_period}] $dc_design_name
+
+
