4c4e5b9aa43bdf9c33b0c8ec8a8fa459f7b8c0c4
Sophia Liu <sophliu@caddy17.stanford.edu>
2020-02-26 21:23:52 -0800

    ringosc

--- ee272-hw6/sim/ringosc/ringosc.sv
+++ ee272-hw6/sim/ringosc/ringosc.sv
@@ -5,4 +5,28 @@ module ringosc (
     output var logic out
 );
     // YOUR IMPLEMENTATION HERE
+    real freq;
+    real period; 
+    logic temp;
+    
+    initial 
+    begin 
+    temp = 0; 
+    end 
+
+    always_comb begin
+        freq = 6.6 * 10 **9 * vdd - 1.3 * 10 **9;
+	if (freq < 1.6 * 10 **9) begin 
+    		freq = 1.6 * 10 **9;
+    	end else if (freq > 4.0 * 10 **9) begin
+        	freq = 4.00 **9 * 10 **9;
+    	end
+    end
+    assign period = 1/freq;
+    
+    always 
+    	#(period/2) temp = ~temp;
+    
+    assign out = temp;
 endmodule
+
