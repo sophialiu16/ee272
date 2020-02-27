7be4300cf7cb2b1fad73bdc5c90fd72284ff9ac0
Sophia Liu <sophliu@caddy17.stanford.edu>
2020-02-27 14:55:29 -0800

    test pass

--- ee272-hw6/sim/filter/filter.sv
+++ ee272-hw6/sim/filter/filter.sv
@@ -6,20 +6,23 @@ module filter #(
     input real in
 );
     // ADD VARIABLE DECLARATIONS HERE AS NECESSARY
-   real prev_out, prev_t, temp;
+   real prev_out, prev_t, prev_in;
     function real out();
          // FILL IN FUNCTION IMPLEMENTATION
-         out = $exp(-($realtime - prev_t)/tau) * prev_out + (1 - $exp(-($realtime - prev_t)/tau)) * in;
+         out = $exp(-($realtime - prev_t)/tau) * prev_out + (1 - $exp(-($realtime - prev_t)/tau)) * prev_in;
     endfunction
 
     initial begin
+          prev_out = out();
           prev_t = $realtime;
-          prev_out = 0;
+          prev_in = 0;
     end
 
     always @(in) begin
+          //prev_t =  $realtime;
+	  prev_out = out();  
+          prev_in = in;
           prev_t = $realtime;
-          prev_out = out();
         // UPDATE INTERNAL STATE VARIABLES AS NECESSARY
     end
 endmodule
