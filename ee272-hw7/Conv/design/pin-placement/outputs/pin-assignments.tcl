5ba71c6b2b89758ea0e8c9af3bf1df71685a3a34
Kavya Sreedhar <skavya@caddy13.stanford.edu>
2020-03-11 18:43:39 -0700

    adding clk to pin placement

--- ee272-hw7/Conv/design/pin-placement/outputs/pin-assignments.tcl
+++ ee272-hw7/Conv/design/pin-placement/outputs/pin-assignments.tcl
@@ -17,7 +17,7 @@
 # If you update pin assignments below you should rerun the pin-placement step 
 # before re-running init step
 
-set pins_left {input_serial_rsc_dat input_serial_rsc_vld weight_serial_rsc_dat weight_serial_rsc_vld output_serial_rsc_rdy paramsIn_rsc_dat paramsIn_rsc_vld}
+set pins_left {clk input_serial_rsc_dat input_serial_rsc_vld weight_serial_rsc_dat weight_serial_rsc_vld output_serial_rsc_rdy paramsIn_rsc_dat paramsIn_rsc_vld}
 
 set pins_right {input_serial_rsc_rdy weight_serial_rsc_rdy output_serial_rsc_dat output_serial_rsc_vld paramsIn_rsc_rdy}
 #set pins_right = []
